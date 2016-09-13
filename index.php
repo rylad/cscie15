504843a5ff6029761d8ab00c4b30eedb56849cbd
rylad <smvalois@gmail.com>
2016-09-13 16:10:06 +0000

    Fixed links

--- index.php
+++ index.php
@@ -8,22 +8,22 @@
  <img src="images/me.jpg" alt="Picture of Me" style="width:300px;height:300px;">
 
  <h2>Project 1 Links</h2>
- <a href="p1.thehomelabs.com">Project 1 Site</a>
+ <a href="http://p1.thehomelabs.com">Project 1 Site</a>
  <br>
  <a href="https://github.com/rylad/cscie15">Project 1 Git Repository</a>
 
  <h2>Project 2 Links</h2>
- <a href="p2.thehomelabs.com">Project 2 Site</a>
+ <a href="http://p2.thehomelabs.com">Project 2 Site</a>
  <br>
  <a href="https://github.com/rylad/cscie15">Project 2 Git Repository</a>
 
  <h2>Project 3 Links</h2>
- <a href="p3.thehomelabs.com">Project 3 Site</a>
+ <a href="http://p3.thehomelabs.com">Project 3 Site</a>
  <br>
  <a href="https://github.com/rylad/cscie15">Project 3 Git Repository</a>
 
  <h2>Project 4 Links</h2>
- <a href="p4.thehomelabs.com">Project 4 Site</a>
+ <a href="http://p4.thehomelabs.com">Project 4 Site</a>
  <br>
  <a href="https://github.com/rylad/cscie15">Project 4 Git Repository</a>
 
