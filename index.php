45012b3c1ca5e6f0d9c7cb115ff424c8da4d38a4
rylad <smvalois@gmail.com>
2016-09-13 11:45:50 +0000

    Fixed formatting

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php 
-	echo "hello world!!!!";
-?>
+<html>
+ <head>
+  <title>csci e-15 projects</title>
+ </head>
+ <body>
+ <h1>Sean Valois</h1>
+ 
+ <img src="images/me.jpg" alt="Picture of Me" style="width:300px;height:300px;">
+
+ <h2>Project 1 Links</h2>
+ <a href="replaceme.com">Project 1 Site</a>
+ <br>
+ <a href="replaceme.com">Project 1 Git Repository</a>
+
+ <h2>Project 2 Links</h2>
+ <a href="replaceme.com">Project 2 Site</a>
+ <br>
+ <a href="replaceme.com">Project 2 Git Repository</a>
+
+ <h2>Project 3 Links</h2>
+ <a href="replaceme.com">Project 3 Site</a>
+ <br>
+ <a href="replaceme.com">Project 3 Git Repository</a>
+
+ <h2>Project 4 Links</h2>
+ <a href="replaceme.com">Project 4 Site</a>
+ <br>
+ <a href="replaceme.com">Project 4 Git Repository</a>
+
+ </body>
+</html>
