59b5f6af649cb294b27043fade45fb712de32e17
rylad <smvalois@gmail.com>
2016-09-21 21:18:03 +0000

    Updating links

--- index.php
+++ index.php
@@ -17,17 +17,17 @@
  <h2>Project 2 Links</h2>
  <a href="http://p2.thehomelabs.com">Project 2 Site</a>
  <br>
- <a href="https://github.com/rylad/cscie15">Project 2 Git Repository</a>
+ <a href="https://github.com/rylad/cscie15-2">Project 2 Git Repository</a>
 
  <h2>Project 3 Links</h2>
  <a href="http://p3.thehomelabs.com">Project 3 Site</a>
  <br>
- <a href="https://github.com/rylad/cscie15">Project 3 Git Repository</a>
+ <a href="https://github.com/rylad/cscie15-3">Project 3 Git Repository</a>
 
  <h2>Project 4 Links</h2>
  <a href="http://p4.thehomelabs.com">Project 4 Site</a>
  <br>
- <a href="https://github.com/rylad/cscie15">Project 4 Git Repository</a>
+ <a href="https://github.com/rylad/cscie15-4">Project 4 Git Repository</a>
 
  </body>
 </html>
